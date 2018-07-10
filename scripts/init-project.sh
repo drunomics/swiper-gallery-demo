d300d3c180bda8543bd7d09056f3d197128c46cd
Mathias Mueller <mathias.mueller@drunomics.com>
2018-07-10 16:38:35 +0200

    Make sure docker container is up.

--- scripts/init-project.sh
+++ scripts/init-project.sh
@@ -8,4 +8,5 @@ source dotenv/loader.sh
 # Run build on the host so we can leverage build caches.
 phapp build
 # Then install in the container.
+./dcp.sh up -d
 ./dcp.sh exec web phapp init --no-build
