daf41534d71f107740a855b8a2650c8e39ef4777
Mathias Mueller <mathias.mueller@drunomics.com>
2018-07-26 18:03:29 +0200

    Run base behat tests.

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,1 +1,15 @@
 #!/usr/bin/env bash
+
+ARGS=""
+
+if [[ $HEADLESS = "1" ]]; then
+  ARGS="--disable-gpu --headless"
+fi
+
+(google-chrome-stable $ARGS --user-data-dir=${HOME}/.google-chrome/behat --remote-debugging-address=127.0.0.1 --remote-debugging-port=9222 )&
+
+cd `dirname $0`/..
+./tests/behat/run.sh
+
+# End with stopping all sub-process; i.e. chrome.
+[[ -z "$(jobs -p)" ]] || kill $(jobs -p)
